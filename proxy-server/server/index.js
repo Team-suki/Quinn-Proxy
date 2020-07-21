d3182dd3e1c8611e58e288148b47f1ccbfa3b611
Quinn Royston <quinn.royston@gmail.com>
2020-07-21 15:16:36 -0700

    refactored proxy to work w/my services load balancer

--- proxy-server/server/index.js
+++ proxy-server/server/index.js
@@ -9,7 +9,7 @@ const rewardsServiceRoute =
   'http://ec2-3-133-92-215.us-east-2.compute.amazonaws.com:3005';
 
 const proxyRouter = {
-  '/api/banner': 'http://localhost:3002',
+  '/api/banner': 'http://sdc-lb-679578692.us-west-1.elb.amazonaws.com/',
 };
 
 app.use(express.static(path.join(__dirname, '../dist')));
